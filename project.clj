f97cbfe7a5ecd08164eb19428440ad41cc08e284
Prasanna Gautam <prasannagautam@gmail.com>
2016-01-18 00:45:35 -0800

    Upgrading to Mesos 0.25

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-mesos "0.22.3-SNAPSHOT"
+(defproject prasincs/clj-mesos "0.25.0-SNAPSHOT"
   :description "A fully-featured Mesos binding for Clojure"
   :url "http://github.com/dgrnbrg/clj-mesos"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.google.protobuf/protobuf-java "2.5.0"]
-                 [org.apache.mesos/mesos "0.20.0"]
+                 [org.apache.mesos/mesos "0.25.0"]
                  [org.clojure/tools.logging "0.2.6"]]
   :global-vars {*warn-on-reflection* true}
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow" "-Xcheck:jni"]
